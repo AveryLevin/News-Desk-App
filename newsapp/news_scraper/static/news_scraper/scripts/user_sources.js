1e6a476993a34663e62564ad9b3aab68302b34c6
Avery Levin <Averylevin@berkeley.edu>
2020-06-10 13:34:29 -0700

    fully implemented source add/remove through vue

--- newsapp/news_scraper/static/news_scraper/scripts/user_sources.js
+++ newsapp/news_scraper/static/news_scraper/scripts/user_sources.js
@@ -18,26 +18,6 @@ var userSourcesDataSamples = {
 var userSourcesData = JSON.parse(document.getElementById('user-source-data').textContent);
 console.log(userSourcesData);
 
-
-Vue.component('curr-source-list', {
-    delimiters: ['[[', ']]'],
-    data: function () {
-        return {
-
-        };
-    },
-    template: `
-    <ul class="source-list">
-        <h4>Your current sources:</h4>
-        <current-source-item 
-        v-for="source in null" 
-        v-bind:name="source.name">
-        </current-source-item>
-
-    </ul>
-    `,
-})
-
 Vue.component('current-source-item', {
     delimiters: ['[[', ']]'],
     data: function () {
@@ -117,28 +97,72 @@ Vue.component('additional-source-item', {
     delimiters: ['[[', ']]'],
     data: function () {
         return {
-            classType: "source-item-light"
-        }
+            classType: "source-item-light",
+            postTo: rootURL + "/user_home/sources"
+        };
     },
     props: {
-        name: String
+        name: String,
+        source: Object
     },
     template: `
     <li :class="classType">
                 <div class=left-side>[[ name ]]</div>
-                
-                <div @click="" id="rm_src" class="edit-source">Remove Source</div>
-            </li>
+                <button 
+                @click="removeSource" 
+                @mouseover="makeItemDark"
+                @mouseleave="makeItemLight"
+                id="rm_src" class="edit-source">Add Source</button>
+    </li>
     `,
     computed: {
 
     },
     methods: {
         makeItemDark: function () {
-            this.classType = "article-item-dark";
+            this.classType = "source-item-dark";
         },
         makeItemLight: function () {
-            this.classType = "article-item-light";
+            this.classType = "source-item-light";
+        },
+        removeSource: function () {
+            console.log("Attempting POST Request to " + this.postTo);
+            let postData = JSON.stringify({
+                action: "Add Source",
+                name: this.name
+            });
+            var self = this;
+            fetch(this.postTo, {
+                method: 'post',
+                credentials: "same-origin",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: postData,
+
+            }).then(
+                function (response) {
+                    if (response.status != 200) {
+                        console.log('Looks like there was a problem. Status Code: ' +
+                            response.status);
+                        return;
+                    }
+
+                    //check response data
+                    response.json().then(function (data) {
+
+                        temp = data.sourceData;
+                        userSourcesData = temp;
+                        console.log("sending:");
+                        console.log(userSourcesData);
+                        self.$emit('sources-changed', userSourcesData);
+                    });
+                }
+            ).catch(function (err) {
+                console.log('Fetch Error :-S', err);
+            });
+
         }
     }
 })
@@ -166,14 +190,25 @@ var app = new Vue({
         }
     },
     template: `
-    <ul class="source-list">
-        <h4>Your current sources:</h4>
-        <current-source-item 
-        v-for="source in this.currentSources" 
-        v-bind:name="source.name"
-        v-on:sources-changed="updateSources">
-        </current-source-item>
+    <div>
+        <ul class="source-list">
+            <h4>Your current sources:</h4>
+            <current-source-item 
+            v-for="source in this.currentSources" 
+            v-bind:name="source.name"
+            v-on:sources-changed="updateSources">
+            </current-source-item>
+        </ul>
 
-    </ul>
+        <ul class="source-list">
+            <h4>Other availabe sources:</h4>
+            <additional-source-item 
+            v-for="source in this.additionalSources" 
+            v-bind:name="source.name"
+            v-on:sources-changed="updateSources">
+            </additional-source-item>
+        </ul>
+    </div>
+
     `,
 });
